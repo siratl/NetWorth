677a577fdea927881120d5dc9950c440fac6d43e
Elisha <elishaubax@gmail.com>
2020-06-15 21:45:15 -0700

    deleted server files

--- server/index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-const server = require('./api/server.js');
-
-const port = process.env.PORT || 4444;
-
-server.listen(port, () => {
-  console.log(`\n == Server Listening on port:${port} == \n`);
-});
